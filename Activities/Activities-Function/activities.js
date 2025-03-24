61edc3b18cab88f804eb6b8340fe63bce6163630
Lorenz Luyahan <lorenzluyahan@filamer.edu.ph>
2025-03-23 20:48:13 -0700

    Activiites

---
+++ Activities/Activities-Function/activities.js
@@ -0,0 +1,19 @@
+function WhatShallWear(temp){
+    if (Temp < 60) {
+        console.log("Wear a jacket");
+    }
+else if( temp < 70) {
+    console.log("Wear a Sweater")
+}
+
+else if(temp < 70){
+    console.log("Wear a T-shirt")
+}
+}
+WhatShallWear(60);
+WhatShallWear(80);
+WhatShallWear(50);
+
+    
+
+
